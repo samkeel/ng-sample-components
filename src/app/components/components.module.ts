dccbd6af431d53add2083ea91c1a78c24aacd21e
Sam Keel <programming@vectors.net.au>
2021-11-09 12:06:27 +0800

    pill filter

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -32,6 +32,7 @@ import { SocialMediaBarComponent } from './social-media-bar/social-media-bar.com
 import { BottomSheetComponent } from './bottom-sheet/bottom-sheet.component';
 import { FieldsetComponent } from './fieldset/fieldset.component';
 import { PillComponent } from './pill/pill.component';
+import { PillFiltersComponent } from './pill-filters/pill-filters.component';
 
 @NgModule({
   declarations: [
@@ -62,7 +63,8 @@ import { PillComponent } from './pill/pill.component';
     SocialMediaBarComponent,
     BottomSheetComponent,
     FieldsetComponent,
-    PillComponent
+    PillComponent,
+    PillFiltersComponent
   ],
   imports: [
     CommonModule,
--- src/app/components/pill/pill.component.ts
+++ src/app/components/pill/pill.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { PillType } from './pill-type.enum';
 
 @Component({
@@ -10,6 +10,14 @@ export class PillComponent {
   @Input() public type: PillType = PillType.Info;
   @Input() public label = '';
   @Input() public iconClasses = '';
-  
+  @Input() public isActive = false;
+  @Input() public canBeSelected = false;
+
+  @Output() public selected = new EventEmitter<{ hasBeenSelected: boolean, label: string }>();
+
+  public toggle(): void {
+    this.isActive = !this.isActive;
+    this.selected.emit({ hasBeenSelected: this.isActive, label: this.label });
+  }
 
 }
